57d571341ea2ae13f44dfa02ad025bf30bb56de3
Lucas Coutinho <lucaspp1@ucl.br>
2021-06-15 21:06:10 -0300

    rota normal

--- ajudaai/lib/main.dart
+++ ajudaai/lib/main.dart
@@ -4,14 +4,7 @@ import 'app/screens/login/Login.dart';
 
 
 void main() {
-  runApp(
-    home: MyApp(), // becomes the route named '/'
-    routes: <String, WidgetBuilder> {
-      '/a': (BuildContext context) => MyPage(title: 'page A'),
-      '/b': (BuildContext context) => MyPage(title: 'page B'),
-      '/c': (BuildContext context) => MyPage(title: 'page C'),
-    },
-  );
+  runApp(MyApp());
   
 }
 
