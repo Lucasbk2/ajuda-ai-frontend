4d70295c239f68e8986ebf63a802dc013df1ba03
Lucasbk2 <lucaslopes@ucl.br>
2021-06-23 16:38:06 -0300

    Navbar mapa

--- ajudaai/lib/app/screens/map/MapView.dart
+++ ajudaai/lib/app/screens/map/MapView.dart
@@ -6,30 +6,33 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapView extends StatefulWidget {
-    MapView({Key key}) : super(key: key);
+  MapView({Key key}) : super(key: key);
 
-    @override
-    _MapView createState() => _MapView();
+  @override
+  _MapView createState() => _MapView();
 }
 
 class _MapView extends State<MapView> {
-  LatLng _center = LatLng(-20.188389,-40.192908);
+  LatLng _center = LatLng(-20.188389, -40.192908);
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-          center: _center,
-          zoom: 13),
-      layers: [
-        TileLayerOptions(
-          urlTemplate: "https://api.mapbox.com/styles/v1/lucasll/ckq01l0re8qij17lmgb5uqb36/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVjYXNsbCIsImEiOiJja3B6eDNvaHUwaG14Mm5yMDZzejFtMXE3In0.2f-bxWD7CQs5KTv0FHvQGw",
-          additionalOptions: {
-            'accessToken' : "pk.eyJ1IjoibHVjYXNsbCIsImEiOiJja3B6eDNvaHUwaG14Mm5yMDZzejFtMXE3In0.2f-bxWD7CQs5KTv0FHvQGw",
-            'id': 'mapbox.mapbox-streets-v8',
-          },
-        )
-      ],
-      );
+    return Scaffold(
+      appBar: AppBar(title: Text("Mapa"),),
+      body: FlutterMap(
+        options: MapOptions(center: _center, zoom: 2),
+        layers: [
+          TileLayerOptions(
+            urlTemplate:
+                "https://api.mapbox.com/styles/v1/lucasll/ckq01l0re8qij17lmgb5uqb36/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVjYXNsbCIsImEiOiJja3B6eDNvaHUwaG14Mm5yMDZzejFtMXE3In0.2f-bxWD7CQs5KTv0FHvQGw",
+            additionalOptions: {
+              'accessToken':
+                  "pk.eyJ1IjoibHVjYXNsbCIsImEiOiJja3B6eDNvaHUwaG14Mm5yMDZzejFtMXE3In0.2f-bxWD7CQs5KTv0FHvQGw",
+              'id': 'mapbox.mapbox-streets-v8',
+            },
+          )
+        ],
+      ),
+    );
   }
 }
