c7f16bfeee4634cd5d14c4d6e8d3edc515a87700
Lucas Coutinho <lucaspp1@ucl.br>
2021-06-30 16:22:45 -0300

    spalsh screen

--- ajudaai/lib/app/services/IUserService.dart
+++ ajudaai/lib/app/services/IUserService.dart
@@ -3,4 +3,9 @@ import 'package:ajudaai/app/shared/models/User.dart';
 abstract class IuserService {
   Future<List<User>> getAllUsers();
   Future<int> insertUser(User user);
+
+  Future<bool> doLogin(String login, String email);
+  Future<bool> doRegister(User user);
+  Future<bool> verifyLogin();
+
 }
--- ajudaai/lib/app/services/impl/UserService.dart
+++ ajudaai/lib/app/services/impl/UserService.dart
@@ -4,6 +4,7 @@ import 'package:ajudaai/app/services/graphql_operation/queries/ReadUsers.dart';
 import 'package:ajudaai/app/services/graphql_operation/mutations/InsertUserMutation.dart';
 import 'package:ajudaai/app/shared/models/User.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserService implements IuserService {
  CustomGraphqlClient _customGraphqlClient = CustomGraphqlClient();
@@ -21,4 +22,37 @@ class UserService implements IuserService {
     print(result);
     return Future.value(1);
   }
+
+  Future<bool> doLogin(String login, String pass) async {
+    final QueryResult result = await _customGraphqlClient.searchQuery(strDoLogin, variables: {
+      "login": login,
+      "pass": pass
+    });
+    print(result);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    try {
+      prefs.setString("nome", result.data["userLogin"]["name"].toString() );
+      prefs.setString("numero", result.data["userLogin"]["number"].toString() );
+      prefs.setString("email", result.data["userLogin"]["email"].toString() );
+      prefs.setString("senha", result.data["userLogin"]["password"].toString() );
+      return Future.value(true);
+    } catch (e) {
+      print(e);
+      return Future.value(false);
+    }
+    
+  }
+
+  Future<bool> doRegister(User user) async {
+    return Future.value(false);
+  }
+
+  Future<bool> verifyLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await Future.delayed(const Duration(seconds: 2), () => "2");
+    bool allEmpty = prefs.getString("email") == null && prefs.getString("senha") == null && prefs.getString("nome") == null && prefs.getString("numero") == null;
+    print(allEmpty);
+    return Future.value(!allEmpty);
+  }
+
 }
